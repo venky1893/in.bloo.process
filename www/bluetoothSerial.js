f24c6e5066ec59eb5c3aa3c2f21058b46bde2db8
venky1893 <venky1893@gmail.com>
2021-04-24 14:57:21 +0530

    Add files via upload

---
+++ www/bluetoothSerial.js
@@ -0,0 +1,106 @@
+cordova.define("in.ebuilders.process.bluetoothSerial", function(require, exports, module) { module.exports = {
+
+    connect: function (macAddress, success, failure) {
+        cordova.exec(success, failure, "PrimeProcessBluetooth", "connect", [macAddress]);
+    },
+
+    // Android only - see http://goo.gl/1mFjZY
+    connectInsecure: function (macAddress, success, failure) {
+        cordova.exec(success, failure, "PrimeProcessBluetooth", "connectInsecure", [macAddress]);
+    },
+
+    disconnect: function (success, failure) {
+        cordova.exec(success, failure, "PrimeProcessBluetooth", "disconnect", []);
+    },
+
+    // list bound devices
+    list: function (success, failure) {
+        cordova.exec(success, failure, "PrimeProcessBluetooth", "list", []);
+    },
+
+    isEnabled: function (success, failure) {
+        cordova.exec(success, failure, "PrimeProcessBluetooth", "isEnabled", []);
+    },
+
+    isConnected: function (success, failure) {
+        dbg("testing prime process feature");
+        cordova.exec(success, failure, "PrimeProcessBluetooth", "isConnected", []);
+    },
+
+    isConnectedOld: function (success, failure) {
+        dbg("testing btserial process feature");
+        cordova.exec(success, failure, "PrimeProcessBluetooth", "isConnected", []);
+    },
+    // the number of bytes of data available to read is passed to the success function
+    available: function (success, failure) {
+        cordova.exec(success, failure, "PrimeProcessBluetooth", "available", []);
+    },
+
+    // read all the data in the buffer
+    read: function (success, failure) {
+        cordova.exec(success, failure, "PrimeProcessBluetooth", "read", []);
+    },
+
+    // reads the data in the buffer up to and including the delimiter
+    readUntil: function (delimiter, success, failure) {
+        cordova.exec(success, failure, "PrimeProcessBluetooth", "readUntil", [delimiter]);
+    },
+
+    // writes data to the bluetooth serial port
+    // data can be an ArrayBuffer, string, integer array, or Uint8Array
+    write: function (data, success, failure) {
+
+        // convert to ArrayBuffer
+        if (typeof data === 'string') {
+            data = stringToArrayBuffer(data);
+        } else if (data instanceof Array) {
+            // assuming array of interger
+            data = new Uint8Array(data).buffer;
+        } else if (data instanceof Uint8Array) {
+            data = data.buffer;
+        }
+
+        cordova.exec(success, failure, "PrimeProcessBluetooth", "write", [data]);
+    },
+
+    // calls the success callback when new data is available
+    subscribe: function (delimiter, success, failure) {
+        cordova.exec(success, failure, "PrimeProcessBluetooth", "subscribe", [delimiter]);
+    },
+
+    // removes data subscription
+    unsubscribe: function (success, failure) {
+        cordova.exec(success, failure, "PrimeProcessBluetooth", "unsubscribe", []);
+    },
+
+    // calls the success callback when new data is available with an ArrayBuffer
+    subscribeRawData: function (success, failure) {
+        cordova.exec(success, failure, "PrimeProcessBluetooth", "subscribeRaw", []);
+    },
+
+    // removes data subscription
+    unsubscribeRawData: function (success, failure) {
+        cordova.exec(success, failure, "PrimeProcessBluetooth", "unsubscribeRaw", []);
+    },
+
+    // clears the data buffer
+    clear: function (success, failure) {
+        cordova.exec(success, failure, "PrimeProcessBluetooth", "clear", []);
+    },
+
+    // reads the RSSI of the *connected* peripherial
+    readRSSI: function (success, failure) {
+        cordova.exec(success, failure, "PrimeProcessBluetooth", "readRSSI", []);
+    }
+
+};
+
+var stringToArrayBuffer = function(str) {
+    var ret = new Uint8Array(str.length);
+    for (var i = 0; i < str.length; i++) {
+        ret[i] = str.charCodeAt(i);
+    }
+    return ret.buffer;
+};
+
+});
---
+++ www/socket.js
@@ -0,0 +1,86 @@
+cordova.define("in.ebuilders.process.Socket", function(require, exports, module) { var exec = require('cordova/exec');
+
+//
+function Socket(){
+    'use strict';
+
+    this.receiveHookName = 'SOCKET_RECEIVE_DATA_HOOK';      // *** Event name to act as "hook" for data receiving
+    this.disconnectedHookName = 'SOCKET_DISCONNECTED';
+    this.pluginRef = 'PrimeProcessSocket';                              // *** Plugin reference for Cordova.exec calls
+}
+
+//
+Socket.prototype.connect = function (successCallback, errorCallback, host, port) {
+    'use strict';
+    exec(successCallback, errorCallback, this.pluginRef, 'connect', [host, port]);
+};
+
+//
+Socket.prototype.disconnect = function (successCallback, errorCallback, connectionId) {
+    'use strict';
+    exec(successCallback, errorCallback, this.pluginRef, 'disconnect', [connectionId]);
+};
+
+//
+Socket.prototype.disconnectAll = function (successCallback, errorCallback) {
+    'use strict';
+    exec(successCallback, errorCallback, this.pluginRef, 'disconnectAll', []);
+};
+               
+//
+Socket.prototype.isConnected = function (connectionId, successCallback, errorCallback) {
+    'use strict';
+    exec(successCallback, errorCallback, this.pluginRef, 'isConnected', [connectionId]);
+};
+
+//
+Socket.prototype.send = function (successCallback, errorCallback, connectionId, data) {
+    'use strict';
+    exec(successCallback, errorCallback, this.pluginRef, 'send', [connectionId, typeof data == 'string' ? data : JSON.stringify(data)]);
+};
+
+Socket.prototype.setMachineName = function (successCallback, errorCallback, machineName) {
+    'use strict';
+    exec(successCallback, errorCallback, this.pluginRef, 'setMachineName', [machineName]);
+};
+
+Socket.prototype.setStartingCavity = function (successCallback, errorCallback, startingCavity) {
+    'use strict';
+    exec(successCallback, errorCallback, this.pluginRef, 'setStartingCavity', [startingCavity]);
+};
+
+//
+Socket.prototype.receive = function (host, port, connectionId, chunk) {
+    'use strict';
+
+    var evReceive = document.createEvent('Events');
+    
+    evReceive.initEvent(this.receiveHookName, true, true);
+    evReceive.metadata = {
+        connection: {
+            id: connectionId,
+            host: host,
+            port: port,
+        },
+        data: chunk
+    };
+
+    document.dispatchEvent(evReceive);
+};
+
+Socket.prototype.disconnectedEvent = function(connectionId) {
+    var evReceive = document.createEvent('Events');
+
+    evReceive.initEvent(this.disconnectedHookName, true, true);
+    evReceive.metadata = {
+        connection: {
+            id: connectionId
+        }
+    };
+
+    document.dispatchEvent(evReceive);
+};
+
+module.exports = new Socket();
+
+});
